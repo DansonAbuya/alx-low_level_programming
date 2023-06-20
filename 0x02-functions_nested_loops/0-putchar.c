2e899c77a583201ac3ebb828df08eff3709c2706
DansonAbuya <dansonabuya@gmail.com>
2023-06-20 10:08:38 -0700

    changed indentation

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,14 @@
-#include <stdio.h>	
+#include <stdio.h>
 
-/*
- * main -Outputs _putchar, followed by a new line
- *
- * Return:Always return 0(success)
- */
+/**
+  * main - Outputs _putchar, followed by a new line
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
-		
 	printf("_putchar");
-	printf("\n");
+	printf('\n');
 
-	return (0);	
-
+	return (0);
 }
