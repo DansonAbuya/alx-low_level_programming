15f96f594005913decc53aae5e19cd59782a3ffc
DansonAbuya <dansonabuya@gmail.com>
2023-06-20 13:05:51 -0700

    xcvbnm

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+  * _islower -finds a lowercase character
+  * @n:is the character to be checked
+  *
+  * Return: 1 for lowercase character or 0 for anything else
+  */
+int _islower(int n)
+{
+	if (n >= 97 && n <= 122)
+	{
+		return (1);
+	}
+
+	return (0);
+}
+
