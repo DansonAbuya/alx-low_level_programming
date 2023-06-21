e471e6021ba6e57227e6287e35651132039c0d9e
DansonAbuya <dansonabuya@gmail.com>
2023-06-21 09:05:56 -0700

    updated

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,13 +4,11 @@
   * add -function that adds two values
   * @n1:The first number 
   * @n2:The second number
-  *
   * Return: Integer value
   */
 int add(int n1, int n2)
 {	
 	return (n1 + n2);
-
 
 }
 
