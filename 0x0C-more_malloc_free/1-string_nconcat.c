6d8dc1bcdb4f7f2623fd15342e9ff9592cef8a8c
DansonAbuya <dansonabuya@gmail.com>
2023-07-13 09:40:09 -0700

    updated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,7 +5,7 @@
   * string_nconcat -  a function that concatenates two strings
   * @s1: string 1
   * @s2: string 2
-  *
+  * @n : initials
   * Return: concanated string
   */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
