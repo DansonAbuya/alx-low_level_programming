e492361253441f14590f3f89e482869bd7b166a3
DansonAbuya <dansonabuya@gmail.com>
2023-07-17 11:44:57 -0700

    updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef _MYHEADER_
 #define _MYHEADER_
+
+/**
+  * struct dog - the  attributes
+  * @name: name of the dog.
+  * @age:  age of the dog.
+  * @owner: owner of the dog.
+  *
+  */ 
 struct dog
 {char *name;
 float age;
