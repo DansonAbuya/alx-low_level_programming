71bc42edf80da4412d13054876a6f168f8e27744
DansonAbuya <dansonabuya@gmail.com>
2023-07-31 13:06:18 -0700

    committed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,51 +1,68 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
-#include <string.h>
 
+
+static listint_t *new_node(const int n);
 /**
-  * insert_nodeint_at_index - ...
-  * @head: ...
-  * @idx: ...
-  * @n: ...
-  *
-  * Return: ...
-  */
+ * insert_nodeint_at_index - function that inserts a new node at a given
+ * position.
+ *
+ * @head: pointer of pointer to a listint_t
+ * @idx: index of the list where the new node should be added
+ * @n: value to set the new node to.
+ *
+ * Return: pointer to the head.
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node1, *iter;
-	unsigned int count_elements = 1;
+	unsigned int i;
+	listint_t *temp, *new;
 
-	if (head)
+	if (!(*head) && idx == 0)
 	{
-		new_node1 = malloc(sizeof(listint_t));
-		if (new_node1 == NULL)
-			return (NULL);
+		*head = new_node(n);
+		return (*head);
+	}
 
-		new_node1->n = n;
-		if (idx > 0)
+	i = 0;
+	temp = *head;
+	while (temp)
+	{
+		if (idx == 0)
 		{
-			iter = *head;
-			while (iter)
-			{
-				if (count_elements == idx)
-				{
-					new_node1->next = iter->next;
-					iter->next = new_node1;
-					return (new_node1);
-				}
-				iter = iter->next;
-				count_elements++;
-			}
-			if (idx > count_elements)
-				return (NULL);
+			*head = new_node(n);
+			(*head)->next = temp;
+			return (*head);
 		}
-		else
+		else if (i == idx - 1)
 		{
-			new_node1->next = *head;
-			*head = new_node1;
+			new = new_node(n);
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
 		}
-		return (new_node1);
+		temp = temp->next;
+		i++;
 	}
 	return (NULL);
 }
+
+
+/**
+ * new_node - creates a new listint_t node.
+ * @n: number to place inside the node.
+ *
+ * Return: pointer to the new node.
+ */
+static listint_t *new_node(const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	return (new);
+}
+
+
