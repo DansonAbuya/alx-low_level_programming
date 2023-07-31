2d09c198a965fa5e6af82a33d1380450f987b375
DansonAbuya <dansonabuya@gmail.com>
2023-07-31 12:57:43 -0700

    upt

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,39 +12,39 @@
   */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *iter;
-	unsigned int count = 1;
+	listint_t *new_node1, *iter;
+	unsigned int count_elements = 1;
 
 	if (head)
 	{
-		new_node = malloc(sizeof(listint_t));
-		if (new_node == NULL)
+		new_node1 = malloc(sizeof(listint_t));
+		if (new_node1 == NULL)
 			return (NULL);
 
-		new_node->n = n;
+		new_node1->n = n;
 		if (idx > 0)
 		{
 			iter = *head;
 			while (iter)
 			{
-				if (count == idx)
+				if (count_elements == idx)
 				{
-					new_node->next = iter->next;
-					iter->next = new_node;
-					return (new_node);
+					new_node1->next = iter->next;
+					iter->next = new_node1;
+					return (new_node1);
 				}
 				iter = iter->next;
-				count++;
+				count_elements++;
 			}
-			if (idx > count)
+			if (idx > count_elements)
 				return (NULL);
 		}
 		else
 		{
-			new_node->next = *head;
-			*head = new_node;
+			new_node1->next = *head;
+			*head = new_node1;
 		}
-		return (new_node);
+		return (new_node1);
 	}
 	return (NULL);
 }
